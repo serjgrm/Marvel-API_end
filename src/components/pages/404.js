3073778cc75f4d58df3724038e1bbb0ac2e16edc
Serhii <grammas56@gmail.com>
2023-02-09 18:36:46 +0200

    Routes

---
+++ src/components/pages/404.js
@@ -0,0 +1,14 @@
+import ErrorMessage from "../errorMessage/ErrorMessage"
+import {Link} from 'react-router-dom'
+
+const Page404 = ()=>{
+    return (
+        <div>
+            <ErrorMessage/>
+
+        </div>
+    )
+}
+
+
+export default Page404;
